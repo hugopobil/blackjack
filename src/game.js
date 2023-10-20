7d5fb20e10e35f42d8a522b1c2620e9261a3fe5c
Hugo Pobil <hpascual@mrhouston.net>
2023-10-20 21:41:17 +0200

    creator message and menu

--- src/game.js
+++ src/game.js
@@ -21,29 +21,39 @@ class Game {
 
         const startMessage = document.createElement("div")
         const howtoplayMessage = document.createElement("div")
+        const exitMessage = document.createElement("div")
+        const creatorMessage = document.createElement("div")
 
-        startMessage.className = "new-game-start"
-        startMessage.textContent = "NEW GAME"
-        startMessage.style.position = "absolute"
-        startMessage.style.width = `300px`;
-        startMessage.style.height = `300px`;
-        startMessage.style.color = "white"
-        startMessage.style.fontSize = "30px"
-        startMessage.style.left = `100px`;
-        startMessage.style.top = `100px`;
+        function create_div_start_menu(element, class_name, name, position, index) {
+            element.className = class_name;
+            element.textContent = name;
+            element.style.position = position;
+            element.style.width = `300px`;
+            element.style.height = `300px`;
+            element.style.color = "white";
+            element.style.fontSize = "30px";
+            element.style.left = `100px`;
+            element.style.top = `${index*100}px`;
+        }
 
-        howtoplayMessage.className = "how-to-play"
-        howtoplayMessage.textContent = "HOW TO PLAY"
-        howtoplayMessage.style.position = "absolute"
-        howtoplayMessage.style.width = `300px`;
-        howtoplayMessage.style.height = `300px`;
-        howtoplayMessage.style.color = "white"
-        howtoplayMessage.style.fontSize = "30px"
-        howtoplayMessage.style.left = `100px`;
-        howtoplayMessage.style.top = `200px`;
+        create_div_start_menu(startMessage, "new-game-start", "NEW GAME", "absolute", 1)
+        create_div_start_menu(howtoplayMessage, "how-to-play", "HOW TO PLAY", "absolute", 2)
+        create_div_start_menu(exitMessage, "settings", "EXIT", "absolute", 3)
+
+        creatorMessage.className = "creators-div"
+        creatorMessage.textContent = "Created by Paula & Hugo"
+        creatorMessage.style.position = "absolute"
+        creatorMessage.style.fontSize = "10px";
+        creatorMessage.style.left = `420px`;
+        creatorMessage.style.top = `670px`;
+        creatorMessage.style.color = `white`;
+
+
 
         this.container.appendChild(howtoplayMessage)
         this.container.appendChild(startMessage)
+        this.container.appendChild(exitMessage)
+        this.container.appendChild(creatorMessage)
 
 
         // const backgroundMusic = document.getElementById('background-music');

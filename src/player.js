ae8a7154c7283dda29f2083e7f0fd76fbefa1967
Hugo Pobil <hpascual@mrhouston.net>
2023-10-25 20:34:40 +0200

    cambio chorra

--- src/player.js
+++ src/player.js
@@ -3,31 +3,8 @@ class Player {
         this.container = container
         this.cash = 1000 // always start with the same amount of cash
         this.cards = []
-        this.setListeners()
     }
 
 
-    setListeners() {
-        window.addEventListener("keydown", (e) => {
-            switch (e.code) {
-                case "KeyF":
-                    console.log("Player bets")
-                    break;
-                default:
-                    return;
-            }
-        });
-
-        // window.addEventListener("keyup", (e) => {
-        //     switch (e.code) {
-        //         case "KeyF":
-        //             break;
-        //         default:
-        //             return;
-        //     }
-        // });
-    }
-
-    // Metodos del jugador
 
 }

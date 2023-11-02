20c31bc114673abce936bed7ecaef411da0c61a0
Hugo Pobil <hpascual@mrhouston.net>
2023-11-02 18:53:52 +0100

    nuevos cambios

--- src/game.js
+++ src/game.js
@@ -1,11 +1,16 @@
 function check_count(cards) {
     let total_count = 0
     for (let card of cards) {
-        console.log(card)
-        if (card["value"] === "K" || card["value"] === "Q" || card["value"] === "J") {total_count += 10}
-        else if (card["value"] === "A" && total_count <= 21) {total_count += 11}
-        else if (card["value"] === "A" && total_count > 21) {total_count += 1}
-        else {total_count += Number(card.value)}}
+        if (card["value"] === "K" || card["value"] === "Q" || card["value"] === "J") {
+            total_count += 10
+        } else if (card["value"] === "A" && total_count <= 21) {
+            total_count += 11
+        } else if (card["value"] === "A" && total_count > 21) {
+            total_count += 1
+        } else {
+            total_count += Number(card.value)
+        }
+    }
     return total_count
 }
 
@@ -18,17 +23,20 @@ class Game {
         this.container = container
         this.player = new Player(this.container);
         this.dealer = new Dealer(this.container);
-        this.bet = 50
+        this.bet = 0
         this.cards = cards
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
         this.x = this.container.offsetWidth
         this.y = this.container.offsetHeight;
-        this.state = "initial-bet";
+        this.state = "";
     }
 
     start() {
-        const bjBoard = document.getElementById ("bj-board")
+        // use this cards
+        this.dealer.cards = [{suit: 'Diamonds', value: 'A'}, {suit: 'Diamonds', value: '4'}]
+
+        const bjBoard = document.getElementById("bj-board")
         bjBoard.style.backgroundImage = 'url("./img/inicio_clean.png")'
 
         const playerControls = document.createElement("div")
@@ -67,20 +75,21 @@ class Game {
 
         const exitButton = document.createElement("button")
         const exitArrow = document.createElement("img")
-        exitArrow.id ="exitArrow"
+        exitArrow.id = "exitArrow"
         exitArrow.src = "./img/back-arrow.png"
         exitButton.id = "exitButton"
         exitButton.textContent = "Back"
         exitButton.appendChild(exitArrow)
         this.container.appendChild(exitButton)
 
-       
+        // PLAYER CASH
 
-        // TODO: Player cash visualization at all times
-        // const playerCash = document.createElement("div")
-        // playerCash.textContent = this.player.cash
-        // playerCash.id = "player-cash"
-        // this.container.appendChild(playerCash)
+        const playerCash = document.createElement("div")
+        playerCash.textContent = `Player cash $${this.player.cash}`
+        playerCash.id = "player-cash"
+        this.container.appendChild(playerCash)
+
+        // CHIP STACK
 
         const chipsStack = document.createElement("div")
         chipsStack.id = "chips-stack"
@@ -91,7 +100,6 @@ class Game {
         for (let chip of chips) {
             let chipButton = document.createElement("button")
             chipButton.id = `chip-${chip}`
-            /*chipsButton.textContent = `${chip}`*/
             chipButton.className = `chip`
             chipsStack.appendChild(chipButton)
 
@@ -99,9 +107,8 @@ class Game {
             coloredChip.style.background = `url('./img/chip-${chip}.png')`;
             coloredChip.style.backgroundSize = 'contain';
             coloredChip.style.borderStyle = "none";
-        
         }
-        
+
         // The player can always exit the game, at any given point
         exitButton.addEventListener("click", () => {
             window.location.reload()
@@ -110,119 +117,134 @@ class Game {
         let player_start_cards_number = 2
         let dealer_start_cards_number = 2
 
-        hitButton.addEventListener("click", () => {
-            if (this.bet !== 0 && this.state === "initial-bet") {
+        this.state = "new-hand"
 
-                for (let i = 0; i < player_start_cards_number; i++) {
-                    this.player.cards.push(this.cards[0])
-                    this.cards.shift()
-                }
-                console.log("Player cards: ", this.player.cards)
-                console.log("Player start count -->", check_count(this.player.cards))
+        if (this.state === "new-hand") {
+            console.log(`The player has $${this.player.cash}`)
+            console.log(`Current bet is $${this.bet}`)
 
-                // for (let i = 0; i < dealer_start_cards_number; i++) {
-                //     this.dealer.cards.push(this.cards[0])
-                //     this.cards.shift()
-                // }
+            // disable the rest of the buttons
+            hitButton.disabled = false
+            standButton.disabled = true
+            doubleButton.disabled = true
+            splitButton.disabled = true
+            surrenderButton.disabled = true
 
-                // use this to test the seguros state
-                this.dealer.cards = [{suit: 'Diamonds', value:'A'}, {suit: 'Diamonds', value:'4'}]
+            const chip5 = document.getElementById("chip-5")
+            chip5.addEventListener("click", () => {
+                this.bet += 5
+                this.player.cash -= 5
+                playerCash.textContent = `Player cash $${this.player.cash}`
+                console.log(`The player has bet $5 and the current bet is ${this.bet}`)
+            })
 
-                console.log("Dealer cards: ", this.dealer.cards)
-                console.log("dealer start count -->", check_count(this.dealer.cards))
+            const chip10 = document.getElementById("chip-10")
+            chip10.addEventListener("click", () => {
+                this.bet += 10
+                this.player.cash -= 10
+                playerCash.textContent = `Player cash $${this.player.cash}`
+                console.log(`The player has bet $10 and the current bet is ${this.bet}`)
+            })
 
-                if (this.player.count === 21) {
-                    console.log("Player has won de round")
-                    this.player.cash += (this.bet * 2) + (this.bet / 2)
-                    this.dealer.cash -= (this.bet * 2) + (this.bet / 2)
-                }
+            const chip25 = document.getElementById("chip-25")
+            chip25.addEventListener("click", () => {
+                this.bet += 25
+                this.player.cash -= 25
+                playerCash.textContent = `Player cash $${this.player.cash}`
+                console.log(`The player has bet $25 and the current bet is ${this.bet}`)
+            })
 
-                // change state, once cards have been given to both player and dealer
-                this.state = "seguros"
-            }
+            const chip50 = document.getElementById("chip-50")
+            chip50.addEventListener("click", () => {
+                this.bet += 50
+                this.player.cash -= 50
+                playerCash.textContent = `Player cash $${this.player.cash}`
+                console.log(`The player has bet $50 and the current bet is ${this.bet}`)
+            })
 
-            // process to let the player decide what happens after delear gets A
-            if (this.state === "seguros") {
+            const chip100 = document.getElementById("chip-100")
+            chip100.addEventListener("click", () => {
+                this.bet += 100
+                this.player.cash -= 100
+                playerCash.textContent = `Player cash $${this.player.cash}`
+                console.log(`The player has bet $100 and the current bet is ${this.bet}`)
+            })
 
-                const safe_message = document.createElement("div")
-                safe_message.className = "safe-message"
-                safe_message.textContent = "The delear has an A! You can continue with your hand by hitting HIT"
-                            + " or press Surrender and get back 50% of your bet now!"
+            hitButton.addEventListener("click", () => {
+                if (this.bet > 0) {
 
-                for (let i = 0; i < dealer_start_cards_number; i++) {
-                    if (this.dealer.cards[i].value === "A") {
-                        console.log('Dealer has an A')
-                        exitButton.disabled = true
-                        this.container.appendChild(safe_message)
+                    // disable all chips button
+                    chip5.disabled = true
+                    chip10.disabled = true
+                    chip25.disabled = true
+                    chip50.disabled = true
+                    chip100.disabled = true
 
-                        // ignore the message and get another card on your hand
-                        hitButton.addEventListener("click", () => {
-                            console.log(`The player continues playing and has a count of ${this.player.count}`)
-                            this.state = "hand-playing"
-                            safe_message.remove()
-                            this.player.cards.push(this.cards[0])
-                            this.cards.shift()
-                            this.player.count = check_count(this.player.cards)
-                            console.log("player count -->", this.player.count)
-                            if (this.player.count > 21) {
-                                this.state = "hand-end"
-                            }
-                        })
-
-                        // stadn button
-                        standButton.addEventListener("click", () => {
-                            console.log(`The player stands and has a count of ${this.player.count}`)
-                        })
-
-                        // double button
-                        doubleButton.addEventListener("click", () => {
-                            console.log(`The player doubles and has a count of ${this.player.count}`)
-                            const current_bet = this.bet
-                            this.player.cash -= current_bet
-                            this.bet += current_bet
-                            this.player.cards.push(this.cards[0])
-                            this.cards.shift()
-                            this.state = "hand-double"
-                        })
-
-                        // split button
-                        splitButton.addEventListener("click", () => {
-                            console.log(`The player splits and has a count of ${this.player.count}`)
-                        })
-
-                        // surrender option to get 50% of bet and play another hand
-                        surrenderButton.addEventListener("click", () => {
-                            console.log(`The player surrenders and has a count of ${this.player.count}`)
-                            this.state = "hand-end"
-                            safe_message.remove()
-                            let value_to_return = this.bet / 2
-                            this.player.cash += value_to_return
-                            this.dealer.cash += value_to_return
-                            this.bet = 0
-                        })
+                    for (let i = 0; i < player_start_cards_number; i++) {
+                        this.player.cards.push(this.cards[0])
+                        this.cards.shift()
                     }
+
+                    console.log("Player cards: ", this.player.cards, "Count: ", check_count(this.player.cards))
+                    console.log("Dealer cards: ", this.dealer.cards, "Count: ", check_count(this.dealer.cards))
+
+                    if (this.player.count === 21) {
+                        console.log("Player has 21 and wins the hand")
+                        this.player.cash += (this.bet * 2) + (this.bet / 2)
+                        this.dealer.cash -= (this.bet * 2) + (this.bet / 2)
+                    }
+                    this.state = "hand-started"
+                }
+            })
+        }
+
+        // the initial bet has been placed, so now the player can ask for cards
+        if (this.state === "hand-started") {
+            const safe_message = document.createElement("div")
+            safe_message.className = "safe-message"
+            safe_message.textContent = "The delear has an A! You can continue with your hand by hitting HIT"
+                + " or press Surrender and get back 50% of your bet now!"
+
+            for (let i = 0; i < dealer_start_cards_number; i++) {
+                if (this.dealer.cards[i].value === "A") {
+                    console.log('Dealer has an A')
+                    exitButton.disabled = true
+                    this.container.appendChild(safe_message)
                 }
             }
 
-            if (this.state === "hand-playing") {
-                exitButton.disabled = false
-            }
+            // continue playing and ignore the message
+            hitButton.addEventListener("click", () => {
+                this.state = "hand-playing"
+                safe_message.remove()
 
-            if (this.state === "hand-end") {
-                exitButton.disabled = false
-            }
+                // give the player a new card
+                this.player.cards.push(this.cards[0])
+                this.cards.shift()
 
-            if (this.state === "hand-double") {
-                exitButton.disabled = false
-            }
+                if (this.player.count > 21) {
+                    this.state = "hand-end"
+                }
+            })
 
-            // esta parte es la ultima que se tiene que programar
-            // ya que se convierten en dos manos, encapsular el codigo en funciones de jugador y deleaer
-            if (this.state === "split-hand") {
-                exitButton.disabled = false
-            }
-        });
+            // stand button
+            standButton.addEventListener("click", () => {
+                this.state = "player-stand"
+            })
 
+            // surrender and end current hand
+            surrenderButton.addEventListener("click", () => {
+                console.log("Player surrenders")
+                safe_message.remove()
+
+                let value_to_return = this.bet / 2
+                this.player.cash += value_to_return
+                this.dealer.cash += value_to_return
+                this.bet = 0
+
+                this.state = "new-hand"
+            })
+        }
 
     }
 }
